a32a53c5ba15533ee1f4a8f026bcc96a7d29b794
Madina <mirnazemle2019@gmail.com>
2022-02-02 00:16:37 +0500

    fix error

--- Validation/order.php
+++ Validation/order.php
@@ -1,55 +1,54 @@
 <?php
-//Task 'Commander's order'
-function validateType($order)
+function validateType($command)
 {
-    if((int) $order != 0){
-        return "ERROR 422, type of order have to be string. ";
+    if((int) $command != 0){
+        return "ERROR 422, type of command have to be string. ";
     }
-    return "Type of order name is ok. ";
+    return "Type of command name is ok. ";
 }
 
-function validateMin(string $order)
+function validateMin(string $command)
 {
     $min = 4;
-    if (strlen($order) <= $min) {
-        return "Very few characters in the order!";
+    if (strlen($command) <= $min) {
+        return "Very few characters in the command!";
     }
     return "Min number of characters are ok!";
 }
 
-function validateMax(string $order)
+function validateMax(string $command)
 {
     $max = 18;
-    if (strlen($order) > $max) {
-        return "Too many characters in the order!"; 
+    if (strlen($command) > $max) {
+        return "Too many characters in the command!"; 
     }
     return "Max number of characters are ok!"; 
 }
 
-function validateInList(string $order, array $order_list)
+function validateInList(string $command, array $command_list)
 {
-    if(!in_array($order, $order_list)){
-        return "There is no such order on the list! ";
+    if(!in_array($command, $command_list)){
+        return "There is no such command on the list! ";
     }
-    return "There is such order on the list!";
+    return "There is such command on the list!";
 }
 
-function orderList(string $order)
+function outputCommand(string $command)
 {
-    $order_list = [
+    $command_list = [
         "anchors aweigh!",
         "set taut!",
         "toss oars!",
         "secure the anchor!",
         "fire!",
     ];
-    
-    $validate_type = validateType($order);
-    $validate_min  = validateMin($order);
-    $validate_max = validateMax($order);
-    $validate_in_list   = validateInList($order, $order_list);
+
+    $validate_type = validateType($command);
+    $validate_min  = validateMin($command);
+    $validate_max = validateMax($command);
+    $validate_in_list   = validateInList($command, $command_list);
     return "$validate_type $validate_min $validate_max $validate_in_list";
 } 
 //Business Logic
-    $order = readline("Give your order, captain!: "); 
-    echo orderList($order);  
+    $command = readline("Give your command, captain!: "); 
+    echo  outputCommand($command);  
