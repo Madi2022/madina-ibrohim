4296920582ec8ea52b7cf29f042e22da96369963
SaidovIbrohim <98034588+SaidovIbrohim@users.noreply.github.com>
2022-02-02 15:17:22 +0500

    Update indеx.php

--- validation/indеx.php
+++ validation/indеx.php
@@ -8,25 +8,25 @@
         return "422: Order's name type is incorrect. Enter letters";
     }
 
-    function validateInteger($Cup_quantity)
+    function validateInteger($cup_quantity)
     {
-        if (is_int($Cup_quantity)) {
+        if (is_int($cup_quantity)) {
             return "200: Cup quantity is correct. Your order is received.";
         }
         return "422: Quantity is incorrect. Enter a numeric value";
     }
 
-    function validateMin($Cup_quantity, $min_value)
+    function validateMin($cup_quantity, $min_value)
     {
-        if (strlen($Cup_quantity) <= $min_value) {
+        if (strlen($cup_quantity) <= $min_value) {
             return "Very few characters in the order!";
         }
         return "Min number of characters is OK!";
     }
 
-    function validateMax($Cup_quantity, $max_value)
+    function validateMax($cup_quantity, $max_value)
     {
-        if (strlen($Cup_quantity) >= $max_value) {
+        if (strlen($cup_quantity) >= $max_value) {
             return "Too many characters  in the order!";
         }
         return "Max number of characters is OK!";
@@ -54,12 +54,12 @@
         return "Order is not on the menu!";
     }
 
-    function orderToBarmen($order_name, $Cup_quantity,  $min_value, $max_value, $cocktails)
+    function orderToBarmen($order_name, $cup_quantity,  $min_value, $max_value, $cocktails)
     {
         $validate_string = validateString($order_name) . "</br>";
-        $validate_item = validateInteger($Cup_quantity) . "</br>";
-        $validate_min = validateMin($Cup_quantity, $min_value) . "</br>";
-        $validate_max = validateMax($Cup_quantity, $max_value) . "</br>";
+        $validate_item = validateInteger($cup_quantity) . "</br>";
+        $validate_min = validateMin($cup_quantity, $min_value) . "</br>";
+        $validate_max = validateMax($cup_quantity, $max_value) . "</br>";
         $validate_in_array = validateInArray($order_name, $cocktails) . "</br>";
         return $validate_string . $validate_item . $validate_min . $validate_max . $validate_in_array;
     }
@@ -68,6 +68,6 @@
     $Cup_quantity = readline("Input the amount of cups you want: ");
     $min_value = 2;
     $max_value = 12;
-    echo orderToBarmen($order_name, $Cup_quantity, $min_value, $max_value, $cocktails);
+    echo orderToBarmen($order_name, $cup_quantity, $min_value, $max_value, $cocktails);
 
 
